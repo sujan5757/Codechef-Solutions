1e6b895a1179861234764a9a4b85ebec748fa104
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-03-21 20:07:18 +0530

    Create Battery Low.c

---
+++ Battery Low.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	int t;
+	scanf("%d",&t);
+	while(t--) {
+	    int n;
+	    scanf("%d",&n);
+	    if(n<=15) {
+	        printf("YES\n");
+	    } else {
+	        printf("NO\n");
+	    }
+	}
+	return 0;
+}
+
