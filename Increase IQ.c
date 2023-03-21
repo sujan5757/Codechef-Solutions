c3392e1d7226e8064d68b51bd067029a2e2f684a
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-03-21 20:07:50 +0530

    Create Increase IQ.c

---
+++ Increase IQ.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int x;
+    scanf("%d\n",&x);
+    int y=x+7;
+	if(y>170)
+	{
+	    printf("YES\n");
+	}
+	else
+	{
+	    printf("NO\n");
+	}
+	return 0;
+}
