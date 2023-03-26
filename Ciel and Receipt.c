8b7b1771bdd3759c2937e75263c067cb398ab45b
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-03-26 08:24:42 +0530

    Create Ciel and Receipt.c

---
+++ Ciel and Receipt.c
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+	int t;cin>>t;
+	int p[]={2048,1024,512,256,128,64,32,16,8,4,2,1};
+	while(t--){
+	    int n,r=0;cin>>n;
+	    for(int i=0;i<12;i++){
+	        while(n>=p[i]){
+	            r++;
+	            n-=p[i];
+        	}
+	    }
+	    cout<<r<<endl;
+	}
+	return 0;
+}
