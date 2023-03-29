e41b09594653c923f052ee4a48c4908f112bca8e
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-03-29 22:34:23 +0530

    Create Couple Game.java

---
+++ Couple Game.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner input = new Sacnner(system.in);
+        int t = input.nextInt();
+        for(int i=0;i<t;i++){
+            int g=input.nextInt();
+            int b=input.nextInt();
+            System.out.println(b-g);
+        }
+    }
+}
