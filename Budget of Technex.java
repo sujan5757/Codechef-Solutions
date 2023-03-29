511653f01745c20e8cdecedd2e5368cedd8f8079
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-03-29 22:34:00 +0530

    Create Budget of Technex.java

---
+++ Budget of Technex.java
@@ -0,0 +1,19 @@
+/* package codechef; // don't place package name! */
+
+
+import java.util.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner input = new Scanner(System.in);
+		int t=input.nextInt();
+		for(int i=0;i<t;i++){
+		    int f=input.nextInt();
+		    System.out.println(f*100);
+		}
+	}
+}
