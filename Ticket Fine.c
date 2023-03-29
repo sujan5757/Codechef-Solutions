5b9d44a39276464f32b61ad56ea4383d2659d666
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-03-29 08:18:52 +0530

    Create Ticket Fine.c

---
+++ Ticket Fine.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void) {
+	int t,a,b,c;
+	scanf("%d",&t);
+	while(t--){
+	    	scanf("%d%d%d",&a,&b,&c);
+	    	int d=b-c;
+	    	printf("%d\n",d*a);
+
+	    
+	}
+	return 0;
+}
+
