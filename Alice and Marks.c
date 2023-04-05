b086b6848b7bcce676c0acc470064ec83305e7dd
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-04-05 08:40:08 +0530

    Create Alice and Marks.c

---
+++ Alice and Marks.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int x,y;
+        scanf("%d\t%d",&x,&y);
+        if(x>=2*y)
+        {
+            printf("YES\n");
+        }else{
+        printf("NO\n");
+        }
+    
+
+
+
+    return 0;
+}
