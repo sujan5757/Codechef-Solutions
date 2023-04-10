bdce49ad0cceb99df619f36549c667b64ded5c0b
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-04-10 18:04:15 +0530

    Create Passes for Fair.c

---
+++ Passes for Fair.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+    int t,k,n;
+    scanf("%d",&t);
+    while(t--)
+    {
+       scanf("%d %d",&n,&k);
+       if(k>n)
+        printf("yes\n");
+        else
+        printf("no\n");
+       
+    }
+	// your code goes here
+	return 0;
+}
+
