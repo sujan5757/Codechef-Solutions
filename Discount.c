d877991141e2b78aa9b54b761d98bf44b935a20b
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-05-19 14:18:12 +0530

    Create Discount.c

---
+++ Discount.c
@@ -0,0 +1,14 @@
+//Sujan P S
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+    int a;
+    scanf("%d",&a);
+    printf("%d\n",100-a);
+    }
+    return 0;
+}
