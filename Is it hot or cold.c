1223a15e6e18f134d9e86611bf0cecb3a2092cbf
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-05-28 21:09:20 +0530

    Create Is it hot or cold.c

---
+++ Is it hot or cold.c
@@ -0,0 +1,17 @@
+//Sujan P S
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+    int a;
+    scanf("%d",&a);
+    if(a>20)
+    printf("HOT\n");
+    else
+    printf("COLD\n");
+    }
+    return 0;
+}
