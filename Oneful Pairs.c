07fbde0d5c4a756a5b641c0cbdc8c954ef9aa861
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-05-31 22:02:17 +0530

    Create Oneful Pairs.c

---
+++ Oneful Pairs.c
@@ -0,0 +1,13 @@
+//Sujan P S
+#include<stdio.h>
+int main()
+{
+    int t,l,v;
+    scanf("%d%d",&t,&l);
+    v=(t+l)+(t*l);
+    if(v==111)
+    printf("YES\n");
+    else
+    printf("NO\n");
+    return 0;
+}
