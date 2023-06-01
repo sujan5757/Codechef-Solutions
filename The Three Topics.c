9b655ef5f03cdf7f318c1a031f4ef3cc1ef12eac
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-06-01 23:53:22 +0530

    Create The Three Topics.c

---
+++ The Three Topics.c
@@ -0,0 +1,13 @@
+//Sujan P S
+#include<stdio.h>
+int main()
+{
+    int t,a,b,c,d;
+    scanf("%d%d%d%d",&t,&a,&b,&c);
+    if(t==c || a==c||b==c)
+    printf("YES\n");
+    else
+    printf("NO\n");
+   
+    return 0;
+}
