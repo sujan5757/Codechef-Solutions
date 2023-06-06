876799556c3bf1716d26c701d1e15f2d2229517c
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-06-06 23:53:21 +0530

    Create Chef in his Office.c

---
+++ Chef in his Office.c
@@ -0,0 +1,16 @@
+//Sujan P S
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+    int a,b;
+    scanf("%d%d",&a,&b);
+  
+    printf("%d\n",((a*4)+b));
+  
+    }
+    return 0;
+}
