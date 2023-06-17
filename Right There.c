94ebe014a7235648b9cdb28e97c8c51962dafbdb
SUJAN P S <99257458+sujan5757@users.noreply.github.com>
2023-06-17 17:58:12 +0530

    Create Right There.c

---
+++ Right There.c
@@ -0,0 +1,17 @@
+//Sujan P S
+#include<stdio.h>
+int main()
+{
+    int t;
+    scanf("%d",&t);
+    while(t--)
+    {
+     int a,b;
+     scanf("%d%d",&a,&b);
+     if(a<=b)
+     printf("YES\n");
+     else
+     printf("NO\n");
+    }
+    return 0;
+}
